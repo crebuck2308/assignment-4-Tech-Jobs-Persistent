4c8dbf30988740c1bc12d7c02a6051cfabd05f98
Chris Rebuck <crebuck.apt@gmail.com>
2021-08-16 18:27:52 -0500

    Copied assignment from assignment repo

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -0,0 +1,37 @@
+package org.launchcode.techjobs.persistent.models;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Employer extends AbstractEntity {
+
+
+    @NotBlank
+    @Size(min=5, max=50)
+    private String location;
+
+    @OneToMany
+    @JoinColumn(name="employer_id")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJob() {
+        return jobs;
+    }
+}
